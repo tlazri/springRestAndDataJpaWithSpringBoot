0df1bcbd21113853e26b3efb0cc1c23b4b873f35
mencinas <mathieu.encinas@alithya.com>
2019-08-27 15:51:20 -0400

    [add] swagger-ui

--- src/main/java/com/example/demo/SpringRestAndDataJpaWithSpringBootApplication.java
+++ src/main/java/com/example/demo/SpringRestAndDataJpaWithSpringBootApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 //@ComponentScan({"controller","service","employee"})
 @SpringBootApplication
+@EnableSwagger2
 public class SpringRestAndDataJpaWithSpringBootApplication {
 
 	public static void main(String[] args) {
