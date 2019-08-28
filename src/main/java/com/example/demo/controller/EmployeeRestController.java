63fdf816e8248196d9e03fc30e18bf7536d79746
mencinas <mathieu.encinas@alithya.com>
2019-08-28 10:29:59 -0400

    [swagger] add swagger docs

--- src/main/java/com/example/demo/controller/EmployeeRestController.java
+++ src/main/java/com/example/demo/controller/EmployeeRestController.java
@@ -3,6 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 
 import com.example.demo.resource.EmployeeResource;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.employee.Employee;
@@ -22,6 +27,8 @@ import com.example.demo.service.EmployeeService;
  */
 
 @RestController
+@Api(tags = {"API pour les opérations CRUD sur les employees."})
+@RequestMapping(value = "/api/employees")
 public class EmployeeRestController {
 
 	@Autowired
@@ -31,39 +38,38 @@ public class EmployeeRestController {
 		this.employeeService = employeeService;
 	}
 
-	@GetMapping("/api/employees")
-	public List<EmployeeResource> getEmployees() {
-		return employeeService.retrieveEmployees();
-	}
 
-	@GetMapping("/api/employees/{employeeId}")
-	public EmployeeResource getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
-		// EmployeeResource employee=employeeService.getEmployee(employeeId);
-		// EmployeeResource checkEntityExists =
+    @ApiOperation(value = "Récupère la liste des employées")
+    public List<EmployeeResource> getEmployees() {
+        return employeeService.retrieveEmployees();
+    }
+
+    @ApiOperation(value = "Récupère un employée par ID")
+    @GetMapping("{employeeId}")
+    public EmployeeResource getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
 		return Checks.checkEntityExists(employeeService.getEmployee(employeeId),
-				"No Employee found for id  = " + employeeId);
+				"No Employee found for id  = " + employeeId);    }
 
-		// return employee;
-	}
+    @ApiOperation(value = "Sau un employée par ID")
+    @PostMapping
+    public void saveEmployee(Employee employee) {
+        employeeService.saveEmployee(employee);
+        System.out.println("Employee Saved Successfully");
+    }
 
-	@PostMapping("/api/employees")
-	public void saveEmployee(Employee employee) {
-		employeeService.saveEmployee(employee);
-		System.out.println("Employee Saved Successfully");
-	}
+    @DeleteMapping("{employeeId}")
+    public void deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        System.out.println("Employee Deleted Successfully");
+    }
 
-	@DeleteMapping("/api/employees/{employeeId}")
-	public void deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
-		employeeService.deleteEmployee(employeeId);
-		System.out.println("Employee Deleted Successfully");
-	}
-
-	@PutMapping("/api/employees/{employeeId}")
-	public void updateEmployee(@RequestBody Employee employee, @PathVariable(name = "employeeId") Long employeeId) {
-		EmployeeResource emp = employeeService.getEmployee(employeeId);
-		if (emp != null) {
-			employeeService.updateEmployee(emp.getEmployee());
-		}
+    @PutMapping("{employeeId}")
+    public void updateEmployee(@RequestBody Employee employee,
+                               @PathVariable(name = "employeeId") Long employeeId) {
+        EmployeeResource emp = employeeService.getEmployee(employeeId);
+        if (emp != null) {
+            employeeService.updateEmployee(emp.getEmployee());
+        }
 
 	}
 
